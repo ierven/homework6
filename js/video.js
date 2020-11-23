9a4454ff884a40b42ba922bfb8fe5d2da171a51a
ierven <ierven@umich.edu>
2020-11-23 18:44:52 -0500

    added js

--- js/video.js
+++ js/video.js
@@ -6,9 +6,10 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.play();
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	video.pause();
 });
 
