5a807b36ef22bdfd431472dd0f0a10df942de203
ierven <ierven@umich.edu>
2020-11-23 19:30:56 -0500

    push

--- js/video.js
+++ js/video.js
@@ -21,3 +21,31 @@ document.querySelector("#faster").addEventListener("click", function() {
 	video.playbackRate /= .9;
 });
 
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) {
+		this.innerHTML = "Mute";
+		video.muted = false;
+	}
+	else {
+		this.innerHTML = "Unmute";
+		video.muted = true;
+	}
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime < video.duration - 5) {
+		video.currentTime += 5;
+	}
+	else {
+		video.currentTime = 0;
+	}
+	video.play()
+});
+
+document.querySelector("#old").addEventListener("click", function() {
+	video.classlist.add("oldTime")
+});
+
+document.querySelector("#original").addEventListener("click", function() {
+	video.classlist.remove("oldTime")
+});
